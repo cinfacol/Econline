d292c15907809d0e982816053b7c3fd763fba1a5
cinfacol <cinfacol@gmail.com>
2024-11-04 22:02:14 -0500

    cambio del puerto del backend archivo .env

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        port: "8080",
+        port: "9090",
         pathname: "/mediafiles/**",
       },
       {
